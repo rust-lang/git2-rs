74a9b0b89f07b663eee15ad86db379ef50fcbabd
Alex Crichton <alex@alexcrichton.com>
2014-08-05 22:51:07 -0700

    Build libgit2 with -fPIC and debug info

--- libgit2/build/cargo.mk
+++ libgit2/build/cargo.mk
@@ -1,6 +1,7 @@
 PWD = $(CURDIR)
 OPTS = -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF \
-       -DCMAKE_BUILD_TYPE=Release -DBUILD_CLAR=OFF
+       -DBUILD_CLAR=OFF -DCMAKE_C_FLAGS=-fPIC \
+       -DCMAKE_BUILD_TYPE=RelWithDebInfo
 all:
 	cmake build/libgit2 -G "Unix Makefiles" -B"$(OUT_DIR)" $(OPTS)
 	make -C "$(OUT_DIR)" -j10
