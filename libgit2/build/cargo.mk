69436118f07f56666ab81352a36dd709da393ee6
Alex Crichton <alex@alexcrichton.com>
2014-08-05 17:43:18 -0700

    Tweak the makefile slightly

--- libgit2/build/cargo.mk
+++ libgit2/build/cargo.mk
@@ -2,6 +2,5 @@ PWD = $(CURDIR)
 OPTS = -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF \
        -DCMAKE_BUILD_TYPE=Release -DBUILD_CLAR=OFF
 all:
-	cd $(DEPS_DIR)
-	(cd $(DEPS_DIR) && cmake $(PWD)/build/libgit2 $(OPTS))
-	(cd $(DEPS_DIR) && cmake --build . -- -j10)
+	cmake build/libgit2 -G "Unix Makefiles" -B"$(OUT_DIR)" $(OPTS)
+	make -C "$(OUT_DIR)" -j10
