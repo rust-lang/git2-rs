7f03391f1dc9258daa30cc0d2d3433e05826f692
Alex Crichton <alex@alexcrichton.com>
2014-08-13 17:09:30 -0700

    Fix cross compilation to 32-bit targets

--- libgit2/build/cargo.mk
+++ libgit2/build/cargo.mk
@@ -1,6 +1,12 @@
+ifneq ($(findstring i686,$(TARGET)),)
+FLAGS=-m32
+else
+FLAGS=-m64
+endif
 OPTS = -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF \
-       -DBUILD_CLAR=OFF -DCMAKE_C_FLAGS=-fPIC \
-       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_EXAMPLES=OFF
+       -DBUILD_CLAR=OFF \
+       -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_EXAMPLES=OFF \
+       -DCMAKE_C_FLAGS="$(FLAGS) -fPIC"
 all:
 	cmake build/libgit2 -G "Unix Makefiles" -B"$(OUT_DIR)" $(OPTS)
 	make -C "$(OUT_DIR)" -j10
