16bff7d39cf74455467727a34cb1d5daddf51db0
Alex Crichton <alex@alexcrichton.com>
2014-08-31 17:25:03 -0700

    Forgot that cred field was already stored

--- src/cred.rs
+++ src/cred.rs
@@ -5,7 +5,6 @@ use {raw, Error};
 /// A structure to represent git credentials in libgit2.
 pub struct Cred {
     raw: *mut raw::git_cred,
-    credtype: raw::git_credtype_t,
 }
 
 impl Cred {
@@ -13,9 +12,8 @@ impl Cred {
     ///
     /// This method is unsafe as there is no guarantee that `raw` is a valid
     /// pointer.
-    pub unsafe fn from_raw(raw: *mut raw::git_cred,
-                           credtype: raw::git_credtype_t) -> Cred {
-        Cred { raw: raw, credtype: credtype }
+    pub unsafe fn from_raw(raw: *mut raw::git_cred) -> Cred {
+        Cred { raw: raw }
     }
 
     /// Create a "default" credential usable for Negotiate mechanisms like NTLM
@@ -25,7 +23,7 @@ impl Cred {
         let mut out = 0 as *mut raw::git_cred;
         unsafe {
             try_call!(raw::git_cred_default_new(&mut out));
-            Ok(Cred::from_raw(out, raw::GIT_CREDTYPE_DEFAULT))
+            Ok(Cred::from_raw(out))
         }
     }
 
@@ -38,7 +36,7 @@ impl Cred {
         unsafe {
             try_call!(raw::git_cred_ssh_key_from_agent(&mut out,
                                                        username.to_c_str()));
-            Ok(Cred::from_raw(out, raw::GIT_CREDTYPE_SSH_KEY))
+            Ok(Cred::from_raw(out))
         }
     }
 
@@ -55,7 +53,7 @@ impl Cred {
                                                 publickey.map(|s| s.to_c_str()),
                                                 privatekey.to_c_str(),
                                                 passphrase.map(|s| s.to_c_str())));
-            Ok(Cred::from_raw(out, raw::GIT_CREDTYPE_SSH_KEY))
+            Ok(Cred::from_raw(out))
         }
     }
 
@@ -68,7 +66,7 @@ impl Cred {
             try_call!(raw::git_cred_userpass_plaintext_new(&mut out,
                                                            username.to_c_str(),
                                                            password.to_c_str()));
-            Ok(Cred::from_raw(out, raw::GIT_CREDTYPE_USERPASS_PLAINTEXT))
+            Ok(Cred::from_raw(out))
         }
     }
 
@@ -81,7 +79,9 @@ impl Cred {
     pub fn raw(&self) -> *mut raw::git_cred { self.raw }
 
     /// Return the type of credentials that this object represents.
-    pub fn credtype(&self) -> raw::git_credtype_t { self.credtype }
+    pub fn credtype(&self) -> raw::git_credtype_t {
+        unsafe { (*self.raw).credtype }
+    }
 
     /// Unwrap access to the underlying raw pointer, canceling the destructor
     pub unsafe fn unwrap(mut self) -> *mut raw::git_cred {
