bf263097cc95c394aad18d82999ab49cd41be3f9
Alex Crichton <alex@alexcrichton.com>
2014-11-07 15:30:13 -0800

    Fix mingw detection

--- libgit2-sys/build.rs
+++ libgit2-sys/build.rs
@@ -24,7 +24,7 @@ fn main() {
 
     let mut cflags = os::getenv("CFLAGS").unwrap_or(String::new());
     let target = os::getenv("TARGET").unwrap();
-    let mingw = target.contains("mingw");
+    let mingw = target.contains("windows-gnu");
     cflags.push_str(" -ffunction-sections -fdata-sections");
 
     if target.contains("i686") {
