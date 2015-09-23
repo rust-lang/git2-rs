c094de7743bb4bfd9b69c55b9548059eb47908e9
Chris Dawes <cmsd2@cantab.net>
2015-09-23 08:26:25 +0100

    replace iter with deref's impl.

--- src/oid_array.rs
+++ src/oid_array.rs
@@ -1,6 +1,6 @@
 //! Bindings to libgit2's raw git_strarray type
 
-use std::ops::{Range,Deref};
+use std::ops::Deref;
 
 use oid::Oid;
 use raw;
@@ -16,12 +16,6 @@ pub struct OidArray {
     raw: raw::git_oidarray,
 }
 
-/// A forward iterator over the Oids of an array, casted to `&Oid`.
-pub struct Iter<'a> {
-    range: Range<usize>,
-    arr: &'a OidArray,
-}
-
 impl OidArray {
     /// Returns None if i is out of bounds.
     pub fn get(&self, i: usize) -> Option<Oid> {
@@ -31,14 +25,6 @@ impl OidArray {
             None
         }
     }
-
-    /// Returns an iterator over the Oids contained within this array.
-    pub fn iter(&self) -> Iter {
-        Iter { range: 0..self.len(), arr: self }
-    }
-
-    /// Returns the number of strings in this array.
-    pub fn len(&self) -> usize { self.raw.count as usize }
 }
 
 impl Deref for OidArray {
@@ -58,20 +44,6 @@ impl Binding for OidArray {
     }
     fn raw(&self) -> raw::git_oidarray { self.raw }
 }
-
-impl<'a> Iterator for Iter<'a> {
-    type Item = Oid;
-    fn next(&mut self) -> Option<Oid> {
-        self.range.next().and_then(|i| self.arr.get(i))
-    }
-    fn size_hint(&self) -> (usize, Option<usize>) { self.range.size_hint() }
-}
-impl<'a> DoubleEndedIterator for Iter<'a> {
-    fn next_back(&mut self) -> Option<Oid> {
-        self.range.next_back().and_then(|i| self.arr.get(i))
-    }
-}
-impl<'a> ExactSizeIterator for Iter<'a> {}
 
 impl Drop for OidArray {
     fn drop(&mut self) {
