25a2ba2fb256b95935d1ff642829d7addb91ba17
Eric Huss <eric@huss.org>
2019-07-13 10:58:54 -0700

    Update libgit2 to latest master.

--- libgit2-sys/lib.rs
+++ libgit2-sys/lib.rs
@@ -331,6 +331,7 @@ pub struct git_remote_callbacks {
     pub push_negotiation: Option<git_push_negotiation>,
     pub transport: Option<git_transport_cb>,
     pub payload: *mut c_void,
+    pub resolve_url: Option<git_url_resolve_cb>,
 }
 
 #[repr(C)]
@@ -384,6 +385,8 @@ pub type git_push_negotiation =
 
 pub type git_push_update_reference_cb =
     extern "C" fn(*const c_char, *const c_char, *mut c_void) -> c_int;
+pub type git_url_resolve_cb =
+    extern "C" fn(*mut git_buf, *const c_char, c_int, *mut c_void) -> c_int;
 
 #[repr(C)]
 pub struct git_push_update {
@@ -2216,7 +2219,7 @@ extern "C" {
         source: *const git_tree,
     ) -> c_int;
     pub fn git_treebuilder_clear(bld: *mut git_treebuilder);
-    pub fn git_treebuilder_entrycount(bld: *mut git_treebuilder) -> c_uint;
+    pub fn git_treebuilder_entrycount(bld: *mut git_treebuilder) -> size_t;
     pub fn git_treebuilder_free(bld: *mut git_treebuilder);
     pub fn git_treebuilder_get(
         bld: *mut git_treebuilder,
