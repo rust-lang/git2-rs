0a529b5347f08f6ef7950269bca5220226ec8d2e
Alex Crichton <alex@alexcrichton.com>
2020-04-01 09:55:51 -0700

    Update to libgit2's 1.0.0 release
    * Enable pkg-config by default now that it has a stable ABI
    * Bump libgit2-sys version number

--- libgit2-sys/build.rs
+++ libgit2-sys/build.rs
@@ -7,14 +7,12 @@ fn main() {
     let https = env::var("CARGO_FEATURE_HTTPS").is_ok();
     let ssh = env::var("CARGO_FEATURE_SSH").is_ok();
 
-    if env::var("LIBGIT2_SYS_USE_PKG_CONFIG").is_ok() {
-        let mut cfg = pkg_config::Config::new();
-        if let Ok(lib) = cfg.atleast_version("0.28.0").probe("libgit2") {
-            for include in &lib.include_paths {
-                println!("cargo:root={}", include.display());
-            }
-            return;
+    let mut cfg = pkg_config::Config::new();
+    if let Ok(lib) = cfg.atleast_version("1.0.0").probe("libgit2") {
+        for include in &lib.include_paths {
+            println!("cargo:root={}", include.display());
         }
+        return;
     }
 
     if !Path::new("libgit2/.git").exists() {
