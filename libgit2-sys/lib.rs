d05202f3e842bb9cc08ed734d65374dd7799f030
Techcable <techcable@techcable.net>
2020-05-01 13:47:47 -0700

    Implement Diff::format_email (#553)
    * Implement Diff::format_email
    
    * Don't hardocde the libgit2 version
    
    diff's format_email_simple test was using this,
    so I just switched to accept anything that looks like a match.
    
    * Attempt to fix Github's formatting complaints
    
    Rustfmt doesn't seem to be working right on my local copy.
    For some reason my version of rustc doesn't seem to support `rustfmt --check`.
    
    * Document Diff::format_email
    
    * Fix compilation error
    
    I should've run 'cargo check' after I formatted by hand.....
    My old code worked just fine.
    
    * Fix formatting issues

--- libgit2-sys/lib.rs
+++ libgit2-sys/lib.rs
@@ -1200,6 +1200,23 @@ pub const GIT_DIFF_BREAK_REWRITES_FOR_RENAMES_ONLY: u32 = 1 << 15;
 pub const GIT_DIFF_FIND_REMOVE_UNMODIFIED: u32 = 1 << 16;
 
 #[repr(C)]
+pub struct git_diff_format_email_options {
+    pub version: c_uint,
+    pub flags: u32,
+    pub patch_no: usize,
+    pub total_patches: usize,
+    pub id: *const git_oid,
+    pub summary: *const c_char,
+    pub body: *const c_char,
+    pub author: *const git_signature,
+}
+
+pub const GIT_DIFF_FORMAT_EMAIL_OPTIONS_VERSION: c_uint = 1;
+
+pub const GIT_DIFF_FORMAT_EMAIL_NONE: u32 = 0;
+pub const GIT_DIFF_FORMAT_EMAIL_EXCLUDE_SUBJECT_PATCH_MARKER: u32 = 1 << 0;
+
+#[repr(C)]
 pub struct git_diff_binary {
     pub contains_data: c_uint,
     pub old_file: git_diff_binary_file,
@@ -3248,6 +3265,16 @@ extern "C" {
         repo: *mut git_repository,
         commit: *const git_oid,
         ancestor: *const git_oid,
+    ) -> c_int;
+
+    pub fn git_diff_format_email(
+        out: *mut git_buf,
+        diff: *mut git_diff,
+        opts: *const git_diff_format_email_options,
+    ) -> c_int;
+    pub fn git_diff_format_email_options_init(
+        opts: *mut git_diff_format_email_options,
+        version: c_uint,
     ) -> c_int;
 
     // patch
