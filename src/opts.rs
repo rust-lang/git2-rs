0313adc9624b19cbfe7a7c432831cdd467a925c4
Josh Triplett <josh@joshtriplett.org>
2020-12-09 07:43:36 -0800

    Provide a binding for GIT_OPT_ENABLE_STRICT_OBJECT_CREATION (#638)
    * Simplify handling of bool option
    
    Rust supports converting a bool to a 0/1 integer via `as`, so we don't
    need to use an if.
    
    * Provide a binding for GIT_OPT_ENABLE_STRICT_OBJECT_CREATION

--- src/opts.rs
+++ src/opts.rs
@@ -2,6 +2,23 @@
 
 use crate::raw;
 
+/// Controls whether or not libgit2 will verify when writing an object that all
+/// objects it references are valid. Enabled by default, but disabling this can
+/// significantly improve performance, at the cost of potentially allowing the
+/// creation of objects that reference invalid objects (due to programming
+/// error or repository corruption).
+pub fn strict_object_creation(enabled: bool) {
+    let error = unsafe {
+        raw::git_libgit2_opts(
+            raw::GIT_OPT_ENABLE_STRICT_OBJECT_CREATION as libc::c_int,
+            enabled as libc::c_int,
+        )
+    };
+    // This function cannot actually fail, but the function has an error return
+    // for other options that can.
+    debug_assert!(error >= 0);
+}
+
 /// Controls whether or not libgit2 will verify that objects loaded have the
 /// expected hash. Enabled by default, but disabling this can significantly
 /// improve performance, at the cost of relying on repository integrity
@@ -10,7 +27,7 @@ pub fn strict_hash_verification(enabled: bool) {
     let error = unsafe {
         raw::git_libgit2_opts(
             raw::GIT_OPT_ENABLE_STRICT_HASH_VERIFICATION as libc::c_int,
-            if enabled { 1 } else { 0 } as libc::c_int,
+            enabled as libc::c_int,
         )
     };
     // This function cannot actually fail, but the function has an error return
