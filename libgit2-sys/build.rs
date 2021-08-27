97091c3f3e170e59b00f669a7f1a2043f4ffd735
Waleed Khan <wkhan@twitter.com>
2021-08-27 12:30:05 -0700

    Add `vendored` feature (#739)
    As per https://github.com/rust-lang/git2-rs/issues/721, some consumers may
    prefer to build `libgit2` themselves to simplify distribution.
    
    To test the vendored version, you can run:
    
    ```
    cargo build --features vendored-libgit2`
    ```
    
    That being said, the test `branch::tests::name_is_valid` fails on macOS. This
    test is currently broken as per
    https://github.com/rust-lang/git2-rs/issues/721#issuecomment-860256600

--- libgit2-sys/build.rs
+++ libgit2-sys/build.rs
@@ -7,10 +7,12 @@ use std::process::Command;
 fn main() {
     let https = env::var("CARGO_FEATURE_HTTPS").is_ok();
     let ssh = env::var("CARGO_FEATURE_SSH").is_ok();
+    let vendored = env::var("CARGO_FEATURE_VENDORED").is_ok();
     let zlib_ng_compat = env::var("CARGO_FEATURE_ZLIB_NG_COMPAT").is_ok();
 
     // To use zlib-ng in zlib-compat mode, we have to build libgit2 ourselves.
-    if !zlib_ng_compat {
+    let try_to_use_system_libgit2 = !vendored && !zlib_ng_compat;
+    if try_to_use_system_libgit2 {
         let mut cfg = pkg_config::Config::new();
         if let Ok(lib) = cfg.atleast_version("1.1.0").probe("libgit2") {
             for include in &lib.include_paths {
