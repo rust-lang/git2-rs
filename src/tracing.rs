50a990fd81bc7f410292bc444dd667630406411c
Stephan Dilly <dilly.stephan@gmail.com>
2021-11-22 21:22:39 +0100

    pass tracing msg as slice instead of &str

--- src/tracing.rs
+++ src/tracing.rs
@@ -57,7 +57,7 @@ impl Binding for TraceLevel {
     }
 }
 
-pub type TracingCb = fn(TraceLevel, &str);
+pub type TracingCb = fn(TraceLevel, &[u8]);
 
 static CALLBACK: AtomicUsize = AtomicUsize::new(0);
 
@@ -76,7 +76,10 @@ extern "C" fn tracing_cb_c(level: raw::git_trace_level_t, msg: *const c_char) {
     let cb = CALLBACK.load(Ordering::SeqCst);
     panic::wrap(|| unsafe {
         let cb: TracingCb = std::mem::transmute(cb);
-        let msg = std::ffi::CStr::from_ptr(msg).to_string_lossy();
-        cb(Binding::from_raw(level), msg.as_ref());
+
+        if !msg.is_null() {
+            let msg = std::ffi::CStr::from_ptr(msg).to_bytes();
+            cb(Binding::from_raw(level), msg);
+        }
     });
 }
