66dd6e5eae41fc04052c9b5d98c8791ae9d2a9d2
Eric Huss <eric@huss.org>
2023-01-20 13:22:38 -0800

    Update to libgit2 1.4.5
    This also reverts b9cbb445e0e08deb990c9bf605b0b0c45e9b462f which bumped
    the submodule to 1.5.0-alpha. That probably should have been handled
    differently. None of the commits in libgit2 between
    1d5b9bd86dccc7347aaadf5e3ab122eed9413404..2a0d0bd19b5d13e2ab7f3780e094404828cbb9a7
    (the previous submodule version and the merge base which was v1.4.0)
    appear to be critical. Most of those were backported to the maint/v1.4
    branch except for the 1.5-specific ones which aren't important to us.

--- libgit2-sys/build.rs
+++ libgit2-sys/build.rs
@@ -15,7 +15,7 @@ fn main() {
     if try_to_use_system_libgit2 {
         let mut cfg = pkg_config::Config::new();
         if let Ok(lib) = cfg
-            .range_version("1.4.0".."1.5.0")
+            .range_version("1.4.5".."1.5.0")
             .print_system_libs(false)
             .probe("libgit2")
         {
@@ -45,21 +45,19 @@ fn main() {
     cp_r("libgit2/include", &include);
 
     cfg.include(&include)
-        .include("libgit2/src/libgit2")
-        .include("libgit2/src/util")
+        .include("libgit2/src")
         .out_dir(dst.join("build"))
         .warnings(false);
 
     // Include all cross-platform C files
-    add_c_files(&mut cfg, "libgit2/src/libgit2");
-    add_c_files(&mut cfg, "libgit2/src/util");
-    add_c_files(&mut cfg, "libgit2/src/libgit2/xdiff");
+    add_c_files(&mut cfg, "libgit2/src");
+    add_c_files(&mut cfg, "libgit2/src/xdiff");
 
     // These are activated by features, but they're all unconditionally always
     // compiled apparently and have internal #define's to make sure they're
     // compiled correctly.
-    add_c_files(&mut cfg, "libgit2/src/libgit2/transports");
-    add_c_files(&mut cfg, "libgit2/src/libgit2/streams");
+    add_c_files(&mut cfg, "libgit2/src/transports");
+    add_c_files(&mut cfg, "libgit2/src/streams");
 
     // Always use bundled http-parser for now
     cfg.include("libgit2/deps/http-parser")
@@ -88,11 +86,11 @@ fn main() {
     // when when COMPILE_PCRE8 is not defined, which is the default.
     add_c_files(&mut cfg, "libgit2/deps/pcre");
 
-    cfg.file("libgit2/src/util/allocators/failalloc.c");
-    cfg.file("libgit2/src/util/allocators/stdalloc.c");
+    cfg.file("libgit2/src/allocators/failalloc.c");
+    cfg.file("libgit2/src/allocators/stdalloc.c");
 
     if windows {
-        add_c_files(&mut cfg, "libgit2/src/util/win32");
+        add_c_files(&mut cfg, "libgit2/src/win32");
         cfg.define("STRSAFE_NO_DEPRECATE", None);
         cfg.define("WIN32", None);
         cfg.define("_WIN32_WINNT", Some("0x0600"));
@@ -104,7 +102,7 @@ fn main() {
             cfg.define("__USE_MINGW_ANSI_STDIO", "1");
         }
     } else {
-        add_c_files(&mut cfg, "libgit2/src/util/unix");
+        add_c_files(&mut cfg, "libgit2/src/unix");
         cfg.flag("-fvisibility=hidden");
     }
     if target.contains("solaris") || target.contains("illumos") {
@@ -162,9 +160,9 @@ fn main() {
     cfg.define("SHA1DC_NO_STANDARD_INCLUDES", "1");
     cfg.define("SHA1DC_CUSTOM_INCLUDE_SHA1_C", "\"common.h\"");
     cfg.define("SHA1DC_CUSTOM_INCLUDE_UBC_CHECK_C", "\"common.h\"");
-    cfg.file("libgit2/src/util/hash/sha1/collisiondetect.c");
-    cfg.file("libgit2/src/util/hash/sha1/sha1dc/sha1.c");
-    cfg.file("libgit2/src/util/hash/sha1/sha1dc/ubc_check.c");
+    cfg.file("libgit2/src/hash/sha1/collisiondetect.c");
+    cfg.file("libgit2/src/hash/sha1/sha1dc/sha1.c");
+    cfg.file("libgit2/src/hash/sha1/sha1dc/ubc_check.c");
 
     if let Some(path) = env::var_os("DEP_Z_INCLUDE") {
         cfg.include(path);
@@ -216,12 +214,8 @@ fn cp_r(from: impl AsRef<Path>, to: impl AsRef<Path>) {
 }
 
 fn add_c_files(build: &mut cc::Build, path: impl AsRef<Path>) {
-    let path = path.as_ref();
-    if !path.exists() {
-        panic!("Path {} does not exist", path.display());
-    }
     // sort the C files to ensure a deterministic build for reproducible builds
-    let dir = path.read_dir().unwrap();
+    let dir = path.as_ref().read_dir().unwrap();
     let mut paths = dir.collect::<io::Result<Vec<_>>>().unwrap();
     paths.sort_by_key(|e| e.path());
 
