2eb8c84dce9be72d2863021f7c41737b31a109ed
Mike Jerred <mjerred.work@gmail.com>
2024-11-05 10:52:10 +0000

    fix: review comments

--- src/merge.rs
+++ src/merge.rs
@@ -402,7 +402,7 @@ impl<'repo> Drop for MergeFileResult<'repo> {
     }
 }
 
-impl<'repo> std::fmt::Display for MergeFileResult<'repo> {
+impl<'repo> std::fmt::Debug for MergeFileResult<'repo> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut ds = f.debug_struct("MergeFileResult");
         if let Some(path) = &self.path() {
