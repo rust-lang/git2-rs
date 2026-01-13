e1ac9e9bffe930b0c8faccdb1c397f6525018d0e
Weihang Lo <me@weihanglo.tw>
2026-01-12 19:43:40 -0500

    fix(sys): repo init new and old options
    * Add `GIT_REPOSITORY_INIT_RELATIVE_GITLINK`
      at libgit2/libgit2@bc737620d
    * Remove `GIT_REPOSITORY_INIT_NO_DOTGIT_DIR`
      at libgit2/libgit2@ca2a241e4

--- libgit2-sys/lib.rs
+++ libgit2-sys/lib.rs
@@ -1245,10 +1245,10 @@ git_enum! {
     pub enum git_repository_init_flag_t {
         GIT_REPOSITORY_INIT_BARE              = 1 << 0,
         GIT_REPOSITORY_INIT_NO_REINIT         = 1 << 1,
-        GIT_REPOSITORY_INIT_NO_DOTGIT_DIR     = 1 << 2,
         GIT_REPOSITORY_INIT_MKDIR             = 1 << 3,
         GIT_REPOSITORY_INIT_MKPATH            = 1 << 4,
         GIT_REPOSITORY_INIT_EXTERNAL_TEMPLATE = 1 << 5,
+        GIT_REPOSITORY_INIT_RELATIVE_GITLINK  = 1 << 6,
     }
 }
 
