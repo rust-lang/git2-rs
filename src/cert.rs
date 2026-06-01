650a0a70200f2d73196e3b8a33341a183df3406d
Daniel Scherzer <daniel.e.scherzer@gmail.com>
2026-06-01 16:34:32 -0700

    [Unsafety minimization] Reduce `unsafe` blocks in src/cert.rs

--- src/cert.rs
+++ src/cert.rs
@@ -92,12 +92,11 @@ impl<'a> Cert<'a> {
 
     fn cast<T>(&self, kind: raw::git_cert_t) -> Option<&T> {
         assert_eq!(mem::size_of::<Cert<'a>>(), mem::size_of::<T>());
-        unsafe {
-            if kind == (*self.raw).cert_type {
-                Some(&*(self as *const Cert<'a> as *const T))
-            } else {
-                None
-            }
+        let current_kind = unsafe { (*self.raw).cert_type };
+        if kind == current_kind {
+            Some(unsafe { &*(self as *const Cert<'a> as *const T) })
+        } else {
+            None
         }
     }
 }
@@ -105,68 +104,66 @@ impl<'a> Cert<'a> {
 impl<'a> CertHostkey<'a> {
     /// Returns the md5 hash of the hostkey, if available.
     pub fn hash_md5(&self) -> Option<&[u8; 16]> {
-        unsafe {
-            if (*self.raw).kind as u32 & raw::GIT_CERT_SSH_MD5 as u32 == 0 {
-                None
-            } else {
-                Some(&(*self.raw).hash_md5)
-            }
+        let kind = unsafe { (*self.raw).kind };
+        if kind as u32 & raw::GIT_CERT_SSH_MD5 as u32 == 0 {
+            None
+        } else {
+            Some(unsafe { &(*self.raw).hash_md5 })
         }
     }
 
     /// Returns the SHA-1 hash of the hostkey, if available.
     pub fn hash_sha1(&self) -> Option<&[u8; 20]> {
-        unsafe {
-            if (*self.raw).kind as u32 & raw::GIT_CERT_SSH_SHA1 as u32 == 0 {
-                None
-            } else {
-                Some(&(*self.raw).hash_sha1)
-            }
+        let kind = unsafe { (*self.raw).kind };
+        if kind as u32 & raw::GIT_CERT_SSH_SHA1 as u32 == 0 {
+            None
+        } else {
+            Some(unsafe { &(*self.raw).hash_sha1 })
         }
     }
 
     /// Returns the SHA-256 hash of the hostkey, if available.
     pub fn hash_sha256(&self) -> Option<&[u8; 32]> {
-        unsafe {
-            if (*self.raw).kind as u32 & raw::GIT_CERT_SSH_SHA256 as u32 == 0 {
-                None
-            } else {
-                Some(&(*self.raw).hash_sha256)
-            }
+        let kind = unsafe { (*self.raw).kind };
+        if kind as u32 & raw::GIT_CERT_SSH_SHA256 as u32 == 0 {
+            None
+        } else {
+            Some(unsafe { &(*self.raw).hash_sha256 })
         }
     }
 
     /// Returns the raw host key.
     pub fn hostkey(&self) -> Option<&[u8]> {
-        unsafe {
-            if (*self.raw).kind & raw::GIT_CERT_SSH_RAW == 0 {
-                return None;
-            }
-            Some(slice::from_raw_parts(
+        let kind = unsafe { (*self.raw).kind };
+        if kind & raw::GIT_CERT_SSH_RAW == 0 {
+            return None;
+        }
+        Some(unsafe {
+            slice::from_raw_parts(
                 (*self.raw).hostkey as *const u8,
                 (*self.raw).hostkey_len as usize,
-            ))
-        }
+            )
+        })
     }
 
     /// Returns the type of the host key.
     pub fn hostkey_type(&self) -> Option<SshHostKeyType> {
-        unsafe {
-            if (*self.raw).kind & raw::GIT_CERT_SSH_RAW == 0 {
-                return None;
-            }
-            let t = match (*self.raw).raw_type {
-                raw::GIT_CERT_SSH_RAW_TYPE_UNKNOWN => SshHostKeyType::Unknown,
-                raw::GIT_CERT_SSH_RAW_TYPE_RSA => SshHostKeyType::Rsa,
-                raw::GIT_CERT_SSH_RAW_TYPE_DSS => SshHostKeyType::Dss,
-                raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_256 => SshHostKeyType::Ecdsa256,
-                raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_384 => SshHostKeyType::Ecdsa384,
-                raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_521 => SshHostKeyType::Ecdsa521,
-                raw::GIT_CERT_SSH_RAW_TYPE_KEY_ED25519 => SshHostKeyType::Ed255219,
-                t => panic!("unexpected host key type {:?}", t),
-            };
-            Some(t)
+        let kind = unsafe { (*self.raw).kind };
+        if kind & raw::GIT_CERT_SSH_RAW == 0 {
+            return None;
         }
+        let raw_type = unsafe { (*self.raw).raw_type };
+        let t = match raw_type {
+            raw::GIT_CERT_SSH_RAW_TYPE_UNKNOWN => SshHostKeyType::Unknown,
+            raw::GIT_CERT_SSH_RAW_TYPE_RSA => SshHostKeyType::Rsa,
+            raw::GIT_CERT_SSH_RAW_TYPE_DSS => SshHostKeyType::Dss,
+            raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_256 => SshHostKeyType::Ecdsa256,
+            raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_384 => SshHostKeyType::Ecdsa384,
+            raw::GIT_CERT_SSH_RAW_TYPE_KEY_ECDSA_521 => SshHostKeyType::Ecdsa521,
+            raw::GIT_CERT_SSH_RAW_TYPE_KEY_ED25519 => SshHostKeyType::Ed255219,
+            t => panic!("unexpected host key type {:?}", t),
+        };
+        Some(t)
     }
 }
 
